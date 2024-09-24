a2b96d7adf539217f201f5aa986cb223515ce34f
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-09-24 23:42:52 +0530

    done till fetching data from an api

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./weather.css"
 import  search_icon from '../assets/search.png'
+import  clear_icon from '../assets/clear.png'
+import  cloud_icon from '../assets/cloud.png'
+import  drizzle_icon from '../assets/drizzle.png'
+import  humidity_icon from '../assets/humidity.png'
+import  rain_icon from '../assets/rain.png'
+import  snow_icon from '../assets/snow.png'
+import  wind_icon from '../assets/wind.png'
 
 function Weather() {
+
+  async function search (city){
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_ID}`;
+    const response  = await fetch (url);
+    const data = await response.json();
+    console.log(data);
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+    search("London");
+  },[]);
+
   return (
     <div className='weather'>
       <div className="search-bar">
         <input type="text" placeholder='search' />
-        <img src= {search_icon} alt=''></img>
+        <img src= {search_icon} alt=''/>
+      </div>
+      <img src= {clear_icon} alt='' className='weather-icon'/>
+      <p className='temperature'>16°C</p>
+      <p className='location'>London</p>
+      <div className="weather-data">
+       <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91%</p>
+            <span>Humidity</span>
+          </div>
+       </div>
+       <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.2 Km/hr</p>
+            <span>Wind Speed</span>
+          </div>
+       </div>
       </div>
     </div>
+
   )
 }
 
